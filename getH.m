be5037d6512a90c697fef2946f40d6586774e39e
Laetus <philipp-froehlich@hotmail.com>
2013-09-20 11:03:04 +0800

    added function

---
+++ getH.m
@@ -0,0 +1,14 @@
+function [ H ] = getH( sigma )
+%GETH Summary of this function goes here
+%   Detailed explanation goes here
+
+H = zeros(5);
+
+for x = -2:2
+    for y = -2:2
+        H(x+3,y+3) = exp(-(x^2 + y^2) / (2 * sigma^2) ) / (2 * pi * sigma^2);
+    end
+end
+
+end
+
