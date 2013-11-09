884f59071aed52344844d5669094271f912a5e3b
Laetus <philipp-froehlich@hotmail.com>
2013-11-09 19:12:02 +0800

    Signed-off-by: Laetus <philipp-froehlich@hotmail.com>

--- Part_3_3.m
+++ Part_3_3.m
@@ -5,9 +5,9 @@ PL = imread('images\corridorl.jpg');
 PR = imread('images\corridorr.jpg');
 PD = imread('images\corridor_disp.jpg');
 
-disp_map = disparity(PL,PR,11,11);
+disp_map = disp_map(PL,PR,11,11);
 
-imshow(-disp_map,[-15 15]);
+imshow(disp_map,[-15 15]);
 title('Is:');
 
 figure;
